f2283d6a39b60707d7fa84b5c5fb6fac5b132367
chenlili <986220912@qq.com>
2019-05-24 19:19:10 +0800

    做相关的一些演练

--- src/page/MyPractice.js
+++ src/page/MyPractice.js
@@ -28,11 +28,29 @@ export default class MyPractice extends Component {
         //     };
         // };
         //es6写法
-        var add = x => (y => x + y);
-        var increment = add(1);
-        var addTen = add(10);
-        console.log(increment(2))  // 3
-        console.log(addTen(2));;  // 12
+        // var add = x => (y => x + y);
+        // var increment = add(1);
+        // var addTen = add(10);
+        // console.log(increment(2))  // 3
+        // console.log(addTen(2));;  // 12
+/******************************************************************/
+        // 函数组合
+        // const compose = (f, g) => (x => f(g(x)));
+        // var toUpperCase = word => word.toUpperCase();
+        // var split = x => (str => str.split(x));
+        // var f = compose(split(' '), toUpperCase);
+        // console.log(f("abcd efgh"));;
+
+
+        let thenable = {
+            then: function(resolve, reject) {
+                resolve(42);
+            }
+        };
+        let p1 = Promise.resolve(thenable);
+        p1.then(function(value) {
+            console.log(value); // 42
+        });
     }
 
     render() {
